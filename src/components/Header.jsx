87ec6e2fb2bf736406fa5b5a903827d9441c21f3
toniripoll <antoni.ripoll.uvigo@gmail.com>
2025-01-09 10:20:57 +0100

    Añadir links a la raiz en el simbolo y nombre empresa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
   return (
     <div className="d-flex justify-content-between align-items-center bg-light mt-0 border-bottom border-1 border-gray p-2 mb-4">
       <div className="d-flex gap-3 align-items-center">
-        <Logo />
-        <p className="fs-4 mb-0">{datos.header.nombre}</p>
+        <Link to={'/'}><Logo /></Link> 
+        <Link to={'/'} className="text-decoration-none"><p className="fs-4 mb-0 text-dark ">{datos.header.nombre}</p></Link>
       </div>
       <div>
         {datos.header.links.map((link, index) => (
