2288cf4b2c1fd682df7fd67522611db072525aef
toniripoll <antoni.ripoll.uvigo@gmail.com>
2025-01-09 14:31:48 +0100

    Añadir el inicio al router

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,11 +2,13 @@ import { Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import Precios from './pages/Precios'
 import Galeria from './pages/Galeria'
+import Inicio from './components/Inicio'
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}>
+        <Route path='/' element={<Inicio />}/>
         <Route path='precios' element={<Precios />}/>
         <Route path='carrousel' element={<Galeria />}/>
       </Route>
