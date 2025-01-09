40210807b4f30360b33a4598d5e149ad2686962b
toniripoll <antoni.ripoll.uvigo@gmail.com>
2025-01-09 09:50:47 +0100

    cambio vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Carpeta de salida (por defecto ya es 'dist')
+    assetsDir: 'assets', // Carpeta donde se guardan los assets estáticos
+    sourcemap: true, // Útil para depuración en producción
+  },
+});
